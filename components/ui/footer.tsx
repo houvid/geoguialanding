69c6bafd68779acc97512f8e219819d0a82c8183
David Gómez <48219567+houvid@users.noreply.github.com>
2023-09-29 09:51:45 -0500

    copy geoguia

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -28,7 +28,7 @@ export default function Footer() {
             </ul>
 
             {/* Copyrights note */}
-            <div className="text-gray-400 text-sm mr-4">&copy;</div>
+            <div className="text-gray-400 text-sm mr-4">&copy; GeoGuia</div>
 
           </div>
 
